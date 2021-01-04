16477f2da57c27ed460521f2facdff6bcf7e0a58
Khuong Tran <dbcgdhh@gmail.com>
2021-01-04 12:04:57 +0700

    Init CompoundInterestTable component

---
+++ components/CompoundInterestTable.vue
@@ -0,0 +1,33 @@
+<template>
+  <table class="table-auto border-collapse">
+    <thead>
+      <tr>
+        <th class="border border-black">Tháng</th>
+        <th class="border border-black">Số tiền</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="(monthFinalAmount, index) in compoundInterestYearResult"
+        :key="index"
+      >
+        <td class="border border-black">{{ index + 1 }}</td>
+        <td class="border border-black">{{ monthFinalAmount }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "CompoundInterestTable",
+
+  data() {
+    return {
+      compoundInterestYearResult: [10000, 20000, 30000]
+    };
+  }
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="container">Home</div>
+  <div class="container mx-auto">
+    <compound-interest-table />
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
+import CompoundInterestTable from "@/components/CompoundInterestTable.vue";
 
 export default Vue.extend({
-  name: 'Home',
-})
+  name: "Home",
+
+  components: {
+    CompoundInterestTable
+  }
+});
 </script>
