c81adb56fdb268d6a5044b08db2eaa8c61f1ef5d
Khuong Tran <dbcgdhh@gmail.com>
2021-01-04 07:00:21 +0700

    Init

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: Roboto, sans-serif;
+  font-size: 16px;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
