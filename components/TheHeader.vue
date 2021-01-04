89a5875b4adf653781a4810f0889d1bd8f22004a
Khuong Tran <dbcgdhh@gmail.com>
2021-01-04 19:56:38 +0700

    Style header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,14 +1,28 @@
 <template>
-  <h1
-    class="mb-8 p-4 uppercase font-bold text-2xl text-center bg-yellow-900 text-white"
-  >
-    Công cụ tính tiền khi đầu tư với lãi kép
-  </h1>
+  <div class="header mb-8 p-4 text-blue-800 text-center">
+    <header class="inline-flex items-center">
+      <img class="header__img" src="@/assets/logo.png" alt="logo" />
+      <h1 class="ml-8 uppercase font-bold text-2xl">
+        Công cụ tính tiền khi đầu tư với lãi kép
+      </h1>
+    </header>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Header"
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.header {
+  border-bottom: 1px solid #bbbbbb;
+}
+
+.header__img {
+  object-fit: cover;
+  width: 50px;
+  height: 50px;
+}
 </style>
