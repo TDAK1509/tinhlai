7ee4e7672a67bb21d131972d210864fae1d769c5
Khuong Tran <dbcgdhh@gmail.com>
2021-01-04 20:10:59 +0700

    Position header content

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="header mb-8 p-4 text-blue-800 text-center">
-    <header class="inline-flex items-center">
+    <header
+      class="mx-auto sm:max-w-screen-lg flex justify-between items-center"
+    >
       <img class="header__img" src="@/assets/logo.png" alt="logo" />
       <h1 class="ml-8 uppercase font-bold text-2xl">
         Công cụ tính tiền khi đầu tư với lãi kép
