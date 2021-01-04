631db014e4760af675f6d81137abfc6e9cebd31a
Khuong Tran <dbcgdhh@gmail.com>
2021-01-04 13:44:31 +0700

    Value binding text fields

--- components/CompoundInterestForm.vue
+++ components/CompoundInterestForm.vue
@@ -1,22 +1,59 @@
 <template>
-  <form>
-    <text-field class="mb-4" label="Số tiền ban đầu" />
-    <text-field class="mb-4" label="Số tiền hàng tháng" />
-    <text-field class="mb-4" label="Lãi suất năm" />
-    <text-field class="mb-4" label="Số năm đầu tư" />
+  <form @submit.prevent="submit">
+    <text-field v-model="initialAmount" class="mb-4" label="Số tiền ban đầu" />
+    <text-field
+      v-model="monthlyAmount"
+      class="mb-4"
+      label="Số tiền hàng tháng"
+    />
+    <text-field v-model="interestRate" class="mb-4" label="Lãi suất năm" />
+    <text-field v-model="years" class="mb-4" label="Số năm đầu tư" />
+    <button
+      class="p-3 border border-black rounded-lg bg-black text-white hover:bg-white hover:text-black"
+    >
+      Tính lãi kép
+    </button>
   </form>
 </template>
 
-<script>
+<script lang="ts">
+import Vue from "vue";
 import TextField from "@/components/TextField.vue";
+import { CompoundInterestInfo } from "@/utils/compound-interest";
 
-export default {
+export default Vue.extend({
   name: "CompoundInterestForm",
 
   components: {
     TextField
+  },
+
+  data() {
+    return {
+      initialAmount: "aaa",
+      monthlyAmount: "",
+      interestRate: "",
+      years: ""
+    };
+  },
+
+  computed: {
+    compoundInterestInfo(): CompoundInterestInfo {
+      return {
+        initialAmount: parseInt(this.initialAmount),
+        monthlyAmount: parseInt(this.monthlyAmount),
+        interestRate: parseInt(this.interestRate),
+        years: parseInt(this.years)
+      };
+    }
+  },
+
+  methods: {
+    submit() {
+      console.log("submitted", this.compoundInterestInfo);
+    }
   }
-};
+});
 </script>
 
 <style>
--- components/TextField.vue
+++ components/TextField.vue
@@ -2,7 +2,13 @@
   <div>
     <label class="font-bold">{{ label }}</label
     ><br />
-    <input class="py-2 px-4 border border-gray-400 rounded-md" type="text" />
+    <input
+      :value="value"
+      class="py-2 px-4 border border-gray-400 rounded-md"
+      type="text"
+      v-bind="$attrs"
+      @input="onInput"
+    />
   </div>
 </template>
 
@@ -10,10 +16,27 @@
 export default {
   name: "TextField",
 
+  inheritAttrs: false,
+
+  model: {
+    prop: "value",
+    event: "input"
+  },
+
   props: {
+    value: {
+      type: String,
+      default: ""
+    },
     label: {
       type: String,
       default: ""
+    }
+  },
+
+  methods: {
+    onInput(event) {
+      this.$emit("input", event.target.value);
     }
   }
 };
