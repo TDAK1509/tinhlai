fba1d3aa05f86dcb6ed468e2fc9fbb1d37d19eab
Khuong Tran <dbcgdhh@gmail.com>
2021-01-04 20:26:46 +0700

    Style footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,18 +1,48 @@
 <template>
-  <footer>
-    <div>
-      Icons made by
-      <a href="https://www.flaticon.com/authors/freepik" title="Freepik"
-        >Freepik</a
-      >
-      from
-      <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-    </div>
+  <footer class="p-4 bg-black text-white">
+    <h4 class="font-bold">Credits</h4>
+    <ul class="list-disc list-inside">
+      <li>
+        Logo made by
+        <a
+          :class="linkTextColor"
+          href="https://www.freelogodesign.org/"
+          target="_blank"
+          >Free logo design</a
+        >
+      </li>
+      <li>
+        Flags icons made by
+        <a
+          :class="linkTextColor"
+          href="https://www.flaticon.com/authors/freepik"
+          title="Freepik"
+          target="_blank"
+          >Freepik</a
+        >
+        from
+        <a
+          :class="linkTextColor"
+          href="https://www.flaticon.com/"
+          title="Flaticon"
+          target="_blank"
+          >www.flaticon.com</a
+        >
+      </li>
+    </ul>
   </footer>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Footer",
+
+  data() {
+    return {
+      linkTextColor: "text-yellow-500"
+    };
+  }
+};
 </script>
 
 <style>
