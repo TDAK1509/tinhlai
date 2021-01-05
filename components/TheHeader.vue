b67c2e58c372df6cd9e46953ad1d67689fc42329
Khuong Tran <dbcgdhh@gmail.com>
2021-01-05 09:32:33 +0700

    Fix header title in mobile

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,39 +1,43 @@
 <template>
   <div class="header mb-8 p-4 text-blue-800 text-center">
-    <header
-      class="mx-auto sm:max-w-screen-lg flex justify-between items-center"
-    >
+    <header class="mx-auto sm:max-w-screen-lg flex flex-wrap items-center">
       <img
-        class="header__img"
+        class="header__img order-1"
         src="@/assets/images/logo-50x50.png"
         alt="logo"
         width="50px"
         height="50px"
       />
 
-      <h1 class="ml-8 uppercase font-bold text-2xl flex-1">
+      <h1
+        class="header__title ml-0 sm:ml-8 mt-4 sm:mt-0 uppercase font-bold text-sm sm:text-2xl flex-grow order-3 sm:order-2"
+      >
         {{ $t("headerTitle") }}
       </h1>
 
-      <nuxt-link :to="switchLocalePath('vn')">
-        <img
-          src="@/assets/images/flag-vn.svg"
-          alt="Tiếng Việt"
-          title="Tiếng Việt"
-          :width="flagWidth"
-          :height="flagHeight"
-        />
-      </nuxt-link>
+      <div
+        class="order-2 sm:order-3 flex-grow sm:flex-grow-0 flex items-center justify-end"
+      >
+        <nuxt-link :to="switchLocalePath('vn')">
+          <img
+            src="@/assets/images/flag-vn.svg"
+            alt="Tiếng Việt"
+            title="Tiếng Việt"
+            :width="flagWidth"
+            :height="flagHeight"
+          />
+        </nuxt-link>
 
-      <nuxt-link :to="switchLocalePath('en')" class="ml-4">
-        <img
-          src="@/assets/images/flag-en.svg"
-          alt="English"
-          title="English"
-          :width="flagWidth"
-          :height="flagHeight"
-        />
-      </nuxt-link>
+        <nuxt-link :to="switchLocalePath('en')" class="ml-4">
+          <img
+            src="@/assets/images/flag-en.svg"
+            alt="English"
+            title="English"
+            :width="flagWidth"
+            :height="flagHeight"
+          />
+        </nuxt-link>
+      </div>
     </header>
   </div>
 </template>
@@ -58,5 +62,11 @@ export default {
 
 .header__img {
   object-fit: cover;
+}
+
+@media screen and (max-width: 640px) {
+  .header__title {
+    flex-basis: 100%;
+  }
 }
 </style>
