f0da8ebe32d557c555d318c0994db0c25708d70c
Khuong Tran <dbcgdhh@gmail.com>
2021-06-28 15:26:47 +0700

    Update flota button background color

--- components/BackToTopButton.vue
+++ components/BackToTopButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="fixed rounded-full w-12 h-12 flex items-center justify-center bg-black hover:bg-gray-500 text-white text-lg"
+    class="fixed rounded-full w-12 h-12 flex items-center justify-center bg-yellow-600 hover:bg-gray-500 text-white text-lg"
     style="right: 1rem; bottom: 1rem"
     v-on="$listeners"
   >
