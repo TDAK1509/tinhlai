a03e05e4f939789c86c07e8fc7f5c3ca22f1ed80
Khuong Tran <dbcgdhh@gmail.com>
2021-06-28 15:20:25 +0700

    Add fontawsome chevron up icon

--- components/BackToTopButton.vue
+++ components/BackToTopButton.vue
@@ -1,10 +1,10 @@
 <template>
   <button
-    class="fixed rounded-full w-12 h-12 bg-black hover:bg-gray-500 text-white text-lg transform rotate-90"
+    class="fixed rounded-full w-12 h-12 flex items-center justify-center bg-black hover:bg-gray-500 text-white text-lg"
     style="right: 1rem; bottom: 1rem"
     v-on="$listeners"
   >
-    &lt;
+    <font-awesome-icon icon="chevron-up" />
   </button>
 </template>
 
