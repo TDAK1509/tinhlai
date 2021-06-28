0aa4423d211d049a74a412b79c3e0faccb7f2df6
Khuong Tran <dbcgdhh@gmail.com>
2021-06-28 15:16:06 +0700

    Add hover

--- components/BackToTopButton.vue
+++ components/BackToTopButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="fixed rounded-full w-12 h-12 bg-blue-800 text-white text-lg transform rotate-90"
+    class="fixed rounded-full w-12 h-12 bg-black hover:bg-gray-500 text-white text-lg transform rotate-90"
     style="right: 1rem; bottom: 1rem"
     v-on="$listeners"
   >
