8021a9b9e59289e8ddbc7453ab6a93d1b5ea6c68
Khuong Tran <dbcgdhh@gmail.com>
2022-09-19 09:17:47 +0700

    Add sendPageView

--- plugins/usergram.ts
+++ plugins/usergram.ts
@@ -1,13 +1,20 @@
 import Vue from "vue";
+import { Usergram } from "@/types/Usergram";
 
 const usergram = (window as any).usergram || [];
 
-Vue.prototype.$usergram = {
+const $usergram: Usergram = {
   sendUse: () => {
     usergram.push(["send", "Ugo97x-1", "cv", "use"]);
   },
 
   sendCopy: () => {
     usergram.push(["send", "Ugo97x-1", "cv", "copy"]);
+  },
+
+  sendPageView: () => {
+    usergram.push(["send", "Ugo97x-1", "pv"]);
   }
 };
+
+Vue.prototype.$usergram = $usergram;
