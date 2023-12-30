7e35b56e72d7b9e6ae88ce669b93ec97a3be0be6
Khuong Tran <dbcgdhh@gmail.com>
2023-12-30 10:17:46 +0700

    Copy style from legacy

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,39 +1,45 @@
 <template>
-  <header class="header">
-    <img
-      class="header__img"
-      src="@/assets/images/logo-50x50.png"
-      alt="logo"
-      width="50px"
-      height="50px"
-    />
+  <div class="header mb-8 p-4 text-blue-800 text-center">
+    <header class="mx-auto sm:max-w-screen-lg flex flex-wrap items-center">
+      <img
+        class="header__img order-1"
+        src="@/assets/images/logo-50x50.png"
+        alt="logo"
+        width="50px"
+        height="50px"
+      />
 
-    <h1 class="header__title text-3xl font-bold underline">
-      {{ $t("headerTitle") }}
-    </h1>
+      <h1
+        class="header__title mt-4 sm:mt-0 uppercase font-bold text-sm sm:text-2xl flex-grow order-3 sm:order-2"
+      >
+        {{ $t("headerTitle") }}
+      </h1>
 
-    <div class="header__languages">
-      <NuxtLink :to="switchLocalePath('vn')">
-        <img
-          src="@/assets/images/flag-vn.svg"
-          alt="Tiếng Việt"
-          title="Tiếng Việt"
-          width="30"
-          height="20"
-        />
-      </NuxtLink>
+      <div
+        class="order-2 sm:order-3 flex-grow sm:flex-grow-0 flex items-center justify-end"
+      >
+        <NuxtLink :to="switchLocalePath('vn')">
+          <img
+            src="@/assets/images/flag-vn.svg"
+            alt="Tiếng Việt"
+            title="Tiếng Việt"
+            width="30"
+            height="20"
+          />
+        </NuxtLink>
 
-      <NuxtLink :to="switchLocalePath('en')">
-        <img
-          src="@/assets/images/flag-en.svg"
-          alt="English"
-          title="English"
-          width="30"
-          height="20"
-        />
-      </NuxtLink>
-    </div>
-  </header>
+        <NuxtLink :to="switchLocalePath('en')" class="ml-4">
+          <img
+            src="@/assets/images/flag-en.svg"
+            alt="English"
+            title="English"
+            width="30"
+            height="20"
+          />
+        </NuxtLink>
+      </div>
+    </header>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -43,18 +49,10 @@ const switchLocalePath = useSwitchLocalePath();
 <style scoped>
 .header {
   border-bottom: 1px solid #bbbbbb;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  text-align: center;
 }
 
 .header__img {
   object-fit: cover;
-}
-
-.header__languages {
-  display: flex;
 }
 
 @media screen and (max-width: 640px) {
