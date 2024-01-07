6040badb7d19bfc91317558f55fb27fd1991fe4d
Khuong Tran <dbcgdhh@gmail.com>
2024-01-07 10:12:24 +0700

    Add TextField

---
+++ components/TextField.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <label class="block font-bold mb-2">{{ label }}</label>
+    <input
+      class="py-2 px-4 w-full border border-gray-400 rounded-md"
+      v-bind="$attrs"
+    />
+  </div>
+</template>
+
+
+<script lang="ts" setup>
+defineOptions({
+  inheritAttrs: false,
+});
+
+defineProps<{
+  label: string;
+}>();
+</script>
