37c79e3dd40a935bdddba87d8332e0c053ff3359
Khuong Tran <dbcgdhh@gmail.com>
2024-01-07 10:32:09 +0700

    i81n

--- i18n.config.ts
+++ i18n.config.ts
@@ -3,9 +3,25 @@ export default defineI18nConfig(() => ({
   messages: {
     vn: {
       headerTitle: "Công cụ tính tiền khi đầu tư với lãi kép",
+      formInitialAmount: "Số tiền ban đầu",
+      formMonthlyAmount: "Số tiền hàng tháng",
+      formInterestRate: "Lãi suất năm (đơn vị %)",
+      formYears: "Số năm đầu tư",
+      formNumberOnly: "Vui lòng chỉ điền số",
+      formCalculate: "Tính lãi",
+      resultTable: "BẢNG KẾT QUẢ:",
+      year: "Năm",
     },
     en: {
       headerTitle: "Compound interest calculator",
+      formInitialAmount: "Initial amount",
+      formMonthlyAmount: "Monthly amount",
+      formInterestRate: "Yearly interest rate (in %)",
+      formYears: "Number of years to invest",
+      formNumberOnly: "Only numbers are allowed",
+      formCalculate: "Calculate",
+      resultTable: "RESULT TABLE:",
+      year: "Year",
     },
   },
 }));
