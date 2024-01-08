1c85b28bf7c352da6837b78984959e6e139cd5ec
Khuong Tran <dbcgdhh@gmail.com>
2024-01-08 09:39:22 +0700

    Add result table

--- app.vue
+++ app.vue
@@ -1,12 +1,40 @@
 <template>
   <TheHeader />
+
   <div id="main" class="px-4 mb-4 sm:px-0 mx-auto sm:max-w-screen-lg">
     <CompoundInterestForm @submit="onCalculate" />
   </div>
+
+  <template v-if="hasResult">
+    <h4
+      class="my-8 p-4 uppercase font-bold text-xl text-center text-blue-800 border-t-2 border-gray-200"
+    >
+      {{ $t("resultTable") }}
+    </h4>
+
+    <div
+      class="mt-4 grid grid-cols-1 sm:grid-cols-3 gap-4 justify-items-center"
+    >
+      <div
+        v-for="(yearResult, index) in compoundInterestResult"
+        :key="index"
+        class="w-full sm:max-w-xs"
+      >
+        <h4 class="uppercase font-bold mb-2">
+          {{ $t("year") }} {{ index + 1 }}
+        </h4>
+        <CompoundInterestTable :data="yearResult" />
+      </div>
+    </div>
+  </template>
 </template>
 
 <script lang="ts" setup>
-function onCalculate(event: unknown) {
-  console.log(event);
+const compoundInterestResult: Ref<number[][]> = ref([]);
+
+const hasResult = computed(() => compoundInterestResult.value.length > 0);
+
+function onCalculate(result: number[][]) {
+  compoundInterestResult.value = result;
 }
 </script>
---
+++ components/compound-interest/CompoundInterestTable.vue
@@ -0,0 +1,26 @@
+<template>
+  <table class="border-collapse w-full">
+    <thead>
+      <tr>
+        <th :class="thClasses">Tháng</th>
+        <th :class="thClasses">Số tiền</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(monthFinalAmount, index) in data" :key="index">
+        <td :class="tdClasses">{{ index + 1 }}</td>
+        <td :class="tdClasses">{{ monthFinalAmount }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  data: number[];
+}>();
+
+const thClasses =
+  "py-2 px-4 border border-blue-800 text-left bg-blue-800 text-white";
+const tdClasses = "py-2 px-4 border border-blue-800";
+</script>
