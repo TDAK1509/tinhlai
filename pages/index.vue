976157af2d6c5c2674577559923b69954dbd0147
Khuong Tran <dbcgdhh@gmail.com>
2024-02-23 12:21:03 +0700

    Conditional visibility for side buttons

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,13 @@
 <template>
   <main id="main" class="px-4 mb-4 sm:px-0 mx-auto sm:max-w-screen-lg">
-    <CopyLinkButton @click="copyUrl()" />
-    <BackToTopButton @click="goToTop()" />
-    <CompoundInterestForm @submit="onCalculate" />
+    <CopyLinkButton v-if="hasResult" @click="copyUrl()" />
+    <BackToTopButton v-show="shouldShowBackToTopButton" @click="goToTop()" />
+    <CompoundInterestForm ref="form" @submit="onCalculate" />
 
     <template v-if="hasResult">
       <h4
         id="result"
+        ref="resultTable"
         class="my-8 p-4 uppercase font-bold text-xl text-center text-cyan-800 border-t-2 border-gray-200"
       >
         {{ $t("resultTable") }}
@@ -37,6 +38,13 @@ const snackbar = useSnackbar();
 const compoundInterestResult: Ref<number[][]> = ref([]);
 
 const hasResult = computed(() => compoundInterestResult.value.length > 0);
+
+const form: Ref<HTMLElement | null> = ref(null);
+const shouldShowBackToTopButton = ref(false);
+
+useIntersectionObserver(form, ([{ isIntersecting }]) => {
+  shouldShowBackToTopButton.value = !isIntersecting;
+});
 
 async function onCalculate(result: number[][]) {
   compoundInterestResult.value = result;
