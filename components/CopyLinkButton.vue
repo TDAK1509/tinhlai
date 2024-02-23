c933485ae24f4a7f48efbd4868cb8cbc6f4a95a7
Khuong Tran <dbcgdhh@gmail.com>
2024-02-23 11:05:47 +0700

    Style copy button

--- components/CopyLinkButton.vue
+++ components/CopyLinkButton.vue
@@ -1,6 +1,7 @@
 <template>
   <button
-    class="fixed rounded-full w-12 h-12 right-4 bottom-8 flex items-center justify-center bg-yellow-600 hover:bg-gray-500 text-white text-lg"
+    class="fixed rounded-full w-12 h-12 right-4 flex items-center justify-center bg-yellow-600 hover:bg-gray-500 text-white text-lg"
+    style="bottom: 5rem"
   >
     <font-awesome-icon icon="fa-solid fa-link" />
   </button>
