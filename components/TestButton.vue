24bea8d2bcc94b6c6e9a8b89c1b6fb3bbc05a8fd
Khuong Tran <dbcgdhh@gmail.com>
2024-03-02 09:58:10 +0700

    Remove TestButton

--- components/TestButton.vue
+++
@@ -1,9 +0,0 @@
-<template>
-   <p>{{ $t('welcome') }}</p>
-  <p>Clicked: {{ clicked }}</p>
-  <button @click="clicked++">Click me</button>
-</template>
-
-<script setup lang="ts">
-const clicked = ref(0);
-</script>
